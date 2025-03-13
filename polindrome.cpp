749b173889ab88bd61da7975a9e6c39947d7645f
an_andrianova <anna.andrianova123@gmail.com>
2025-03-13 16:11:10 +0700

    Добавлена функция проверки палиндрома

---
+++ polindrome.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+bool isPalindrome(const std::string& str) {
+    std::string cleanedStr;
+    
+    // Убираем неалфавитные символы и приводим к нижнему регистру
+    for (char ch : str) {
+        if (std::isalnum(ch)) {
+            cleanedStr += std::tolower(ch);
+        }
+    }
+    
+    std::string reversedStr = cleanedStr;
+    std::reverse(reversedStr.begin(), reversedStr.end());
+    
+    return cleanedStr == reversedStr;
+}
+
+int main() {
+    std::string input;
+    std::cout << "Введите строку: ";
+    std::getline(std::cin, input);
+
+    if (isPalindrome(input)) {
+        std::cout << "Строка является палиндромом." << std::endl;
+    } else {
+        std::cout << "Строка не является палиндромом." << std::endl;
+    }
+
+    return 0;
+}
